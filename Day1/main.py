254aeeda4a6c469bc21d338dca1ca51746afc742
kursatfndk <147005854+kursatfndk@users.noreply.github.com>
2024-07-02 02:49:47 +0300

    Add files via upload

---
+++ Day1/main.py
@@ -0,0 +1,36 @@
+#camelCase
+#snake_case method, değişken
+#PascalCase sınıf
+
+#PEP8 Python Standarları ***
+#Boelan değerler True/False
+# 18 yaşının üzerindeyse porn değilse elma yazdır. Bunu sex isimli fonksiyon ile yapalım.
+
+username = "kursatemrefndk"
+password = "123456"
+age = 22
+gender = 0 # Eğer 0 ise Erkek 1 ise Kadın 2 ise Diğer
+is_active = True
+
+customer_data = f"{username} {password} {age} {gender}"
+
+def get_gender(gender):
+  gender_text = ""
+  if gender == 0:
+    gender_text = "Erkek"
+  elif gender == 1:
+    gender_text = "Kadın"
+  else:
+    gender_text = "Diğer"
+  print(f"Cinsiyet: {gender_text}")
+
+if (username == "kursatemrefndk" and password == "123456"):
+  if(is_active):
+    print("Welcome!")
+    print(f"Username: {username}")
+    get_gender(gender)
+  else:
+    print("User exist! But inactive!")
+else:
+  print("Password wrong! Try again!")
+
