82a486ab6b9e8b67d3f03a08aa01d626cc5ab701
kursatfndk <147005854+kursatfndk@users.noreply.github.com>
2024-07-04 19:52:51 +0300

    Add files via upload

---
+++ Day2/main (1).py
@@ -0,0 +1,29 @@
+video_types = ["History", "Science", "politics", "religion"]
+# Create a list of video types
+
+print(video_types)
+print(video_types[0])
+print(video_types[1])
+print(video_types[2])
+print(video_types[3])
+
+print(len(video_types)) #lengt
+
+video_types[0] = "Historycal"
+print(video_types)
+
+#alias
+for video in video_types:
+  print(video)
+
+#loops
+for i in range(len(video_types)):
+  print(video_types[i])
+
+for i in range(10):
+ print(i)
+
+for i in range(3,10):
+ print(i)
+for i in range(0,10,2):
+  print(i)
